7cac65192a42e80d8fe23718e683c678ed4dc90f
prabhath147258 <89913602+prabhath147258@users.noreply.github.com>
2021-09-07 18:42:07 +0530

    Add files via upload

---
+++ PrimeFinder.java
@@ -0,0 +1,21 @@
+package com.tc.training.java.prime;
+
+public class PrimeFinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=2;i<=100;i++) {
+			int check=prime(i);
+			if (check==1)
+				System.out.print(i+" ");
+		}
+	}
+	public static int prime(int a) {
+		for(int i=2;i<=a/2;i++) {
+			if(a%i==0)
+				return 0;
+		}
+		return 1;
+	}
+
+}
